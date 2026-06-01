150f8b6738343ad2a7bae32b077e579e5cb4b6c5
Your Name <your@email.com>
2026-06-01 08:19:43 +0545

    Connecting static files

--- todo_main/settings.py
+++ todo_main/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,5 +119,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-   BASE_DIR / 'staticfiles',
+    BASE_DIR / 'todo_main' / 'static',
 ]
